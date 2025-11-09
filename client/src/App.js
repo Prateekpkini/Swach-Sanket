fee7dcddc9c12a35f6d56369ce5de24bb783c6e6
YASHMITHA A <yashmithavagman@gmail.com>
2025-11-09 10:07:15 +0530

    updated

--- client/src/App.js
+++ client/src/App.js
@@ -22,9 +22,9 @@ function App() {
           <Route
             path="/dashboard"
             element={
-              <ProtectedRoute>
+              // <ProtectedRoute>
                 <Dashboard />
-              </ProtectedRoute>
+              // </ProtectedRoute>
             }
           />
 
